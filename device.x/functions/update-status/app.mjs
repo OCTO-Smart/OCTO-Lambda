2c57f40bfdb5e5a58e3ef2e235d7815dc4af2387
Cahit Taskin <ctaskin@imobilecode.com>
2025-02-25 11:23:56 +0300

    RDS update implemented

--- device.x/functions/update-status/app.mjs
+++ device.x/functions/update-status/app.mjs
@@ -1,11 +1,25 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import pkg from 'pg';
+const { Client } = pkg;
 
-// S3 client oluştur
-const s3 = new S3Client({ region: "eu-central-1" });  // Kendi bölgeni ekle
+import * as fs from 'fs'; 
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// S3 bucket adı
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const REGION = "eu-central-1"
 const BUCKET_NAME = "ivocdevices";
 
+const DB_HOST = "ivocdev.cxiqcy2uo0c3.eu-central-1.rds.amazonaws.com";
+const DB_NAME = "ivocdev";
+const DB_USER = "ivoc";
+const DB_PASSWORD = "ivocDevDB!";
+
+
+// S3 client oluştur
+const s3 = new S3Client({ region: REGION }); 
 
 /**
  * Sample Lambda function which mocks the operation of checking the current price of a stock.
@@ -23,6 +37,29 @@ export const lambdaHandler = async (event, context) => {
 
         // Event'ten 'body' kısmını al (API Gateway kullanılıyorsa body string olabilir)
         const data = event.body ? JSON.parse(event.body) : event;
+
+        if (data.dn == "IVOC2") {
+            const client = new Client({
+                host: DB_HOST,
+                database: DB_NAME,
+                user: DB_USER,
+                password: DB_PASSWORD,
+                port: 5432,
+                ssl: {
+                    rejectUnauthorized: true, // Ensures SSL verification
+                    ca: fs.readFileSync(path.join(__dirname, 'eu-central-1-bundle.pem')).toString()
+                }
+            });
+
+            await client.connect();
+
+            await client.query(
+                "UPDATE device_status SET status = $1, updated_at = NOW() WHERE device_id = $2",
+                [JSON.stringify(data), 2]
+            );
+
+            await client.end();
+        }
 
         // JSON'u bir dosya olarak kaydetmek için ad oluştur
         const fileName = `${data.dn}.json`;
